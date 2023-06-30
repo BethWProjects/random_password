ad439c9422465201c9fae87b82e0d3689be5b306
Beth Wilson <bethwilson777@gmail.com>
2023-06-30 10:56:30 -0600

    Added dark/light mode

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let passwordElOne = document.querySelector(".password-one");
 let passwordElTwo = document.querySelector(".password-two");
+let themeSwitch = document.querySelector("#theme-switch");
+let toggleDark = true;
 
-console.log(passwordElOne);
+
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
@@ -27,3 +29,16 @@ function generatePassword() {
 }
 }
 
+function setLightDarkMode() {
+    let body = document.body;
+    body.classList.toggle('light-mode');
+    let heading = document.querySelector('h2');
+    heading.classList.toggle('light-mode-description');
+    if (toggleDark === true) {
+        themeSwitch.textContent = "Dark Mode";
+        toggleDark = false;
+    } else {
+        themeSwitch.textContent = "Light Mode";
+        toggleDark = true;
+    }
+}
